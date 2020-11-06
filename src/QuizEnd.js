834e96cd1f553e4f9aed1e61ca5aa25fd19cbe8b
David Hubick <dhubick@sgi.sk.ca>
2020-11-06 14:56:21 -0600

    Module 6, try 1

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -6,7 +6,7 @@ class QuizEnd extends Component {
             <div>
                 <p>Thanks for playing!</p>
                 <a href=''>Reset Quiz</a>
-            </div>
+             </div>
         )
     }
 }
