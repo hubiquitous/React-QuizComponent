e1a80c9cb9742569dbd7e773298893a8458592fb
David Hubick <dhubick@sgi.sk.ca>
2020-11-06 15:11:37 -0600

    Module 6, Try 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
 
     showNextQuestion() {
         this.setState((state) => {
-            return {quiz_position: state.quiz_question + 1}
+            return { quiz_position: state.quiz_question + 1 }
         })
     }
 
@@ -25,7 +25,7 @@ class Quiz extends Component {
                     <QuizEnd /> :
                     <QuizQuestion quiz_question={quizData.quiz_questions
                         [this.state.quiz_position- 1]} showNextQuestionHandler=
-                        {this.showNextQuestion.bind(this)}/>}
+                        {this.showNextQuestion.bind(this)} />}
             </div>
         )
     }
