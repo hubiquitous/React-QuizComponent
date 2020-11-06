2b3c2420a4d5ba3912f09059c0994f40bb918897
David Hubick <dhubick@sgi.sk.ca>
2020-11-06 13:37:43 -0600

    Module 4, Try 1 again

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
-    render (
+    render () {
         <li>
             <button></button>
         </li>
-    )
 
+    }
 }
 
 export default QuizQuestionButton
